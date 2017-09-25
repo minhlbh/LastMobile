9a6345eb5fa50a199859a85528031d3434692774
tthanhphuc1711@ggmail.ccom <tthanhphuc1711@ggmail.ccom>
2017-09-25 11:45:51 +0700

    fbsdk_ios

--- ios/TruongKhoa/AppDelegate.m
+++ ios/TruongKhoa/AppDelegate.m
@@ -8,12 +8,13 @@
  */
 
 #import "AppDelegate.h"
-
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
 @implementation AppDelegate
 
+  
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
@@ -25,7 +26,10 @@
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+                           didFinishLaunchingWithOptions:launchOptions];
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
@@ -33,5 +37,15 @@
   [self.window makeKeyAndVisible];
   return YES;
 }
-
+   - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+   sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+     
+     BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                                   openURL:url
+                                                         sourceApplication:sourceApplication
+                                                                annotation:annotation
+                     ];
+     // Add any custom logic here.
+     return handled;
+   }
 @end
